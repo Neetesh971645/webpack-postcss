360502013a0c003d62af347193cc22eb22f00a4f
Neetesh Keshari <neetesh.keshari@akqa.com>
2018-04-14 15:07:58 +0530

    WebPack PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+//All the post CSS Plugins;
+//
+module.exports = {
+	plugins: {
+		'rucksack-css': {},		//Created as object, just to pass some options [Special configuration for plugins]
+		'lost': {},
+		'autoprefixer': {},
+		'cssnano': {}
+	}
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+//import './css/main.css';
+import './scss/main.scss';
+
+import {hello, sup} from './js/module'; //to export JS file, you don't need to JS extension.
+
+hello();
+sup();
